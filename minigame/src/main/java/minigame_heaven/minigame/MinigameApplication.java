3769dcd40bbd19c8503fc80df50937ed05afeffb
stzgkg <154657737+stzgkg@users.noreply.github.com>
2024-05-04 17:54:49 +0900

    first upload of spring boot

---
+++ minigame/src/main/java/minigame_heaven/minigame/MinigameApplication.java
@@ -0,0 +1,13 @@
+package minigame_heaven.minigame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinigameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinigameApplication.class, args);
+	}
+
+}
---
+++ minigame/src/test/java/minigame_heaven/minigame/MinigameApplicationTests.java
@@ -0,0 +1,13 @@
+package minigame_heaven.minigame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinigameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
