b338aeae177b2f76fe4cf6cf3e09c079bf2086de
stzgkg <154657737+stzgkg@users.noreply.github.com>
2024-07-20 00:44:07 +0900

    적 구현(1)
    1. Enemy 클래스를 만들어 적 구현
    
    추가해야할 것
    1. 적과의 충돌시 이벤트 구현
    2. 점수 구현
    3. 점수 처리 관련 백엔드 처리

--- minigame/src/main/resources/static/dodge/dodge.js
+++ minigame/src/main/resources/static/dodge/dodge.js
@@ -8,11 +8,78 @@ let prevPosY = 100;
 const speed = 5;
 const keys = {};
 
+class Enemy {
+  constructor() {
+    this.spawn();
+  }
+
+  spawn() {
+    // 화면 밖 무작위 지점에 적 생성
+    const side = Math.floor(Math.random() * 4);
+    switch (side) {
+      case 0: // 왼쪽
+        this.posX = -20;
+        this.posY = Math.random() * canvas.height;
+        break;
+      case 1: // 오른쪽
+        this.posX = canvas.width + 20;
+        this.posY = Math.random() * canvas.height;
+        break;
+      case 2: // 위쪽
+        this.posX = Math.random() * canvas.width;
+        this.posY = -20;
+        break;
+      case 3: // 아래쪽
+        this.posX = Math.random() * canvas.width;
+        this.posY = canvas.height + 20;
+        break;
+    }
+    this.speed = 2;
+
+    this.dx = posX - this.posX;
+    this.dy = posY - this.posY;
+    this.distance = Math.sqrt(this.dx * this.dx + this.dy * this.dy);
+
+    this.dx = (this.dx / this.distance) * this.speed;
+    this.dy = (this.dy / this.distance) * this.speed;
+  }
+
+  update() {
+    this.posX += this.dx;
+    this.posY += this.dy;
+
+    // 적이 화면을 벗어나는지 체크하고 무작위 위치로 이동
+    if (
+      this.posX < -20 ||
+      this.posX > canvas.width + 20 ||
+      this.posY < -20 ||
+      this.posY > canvas.height + 20
+    ) {
+      this.spawn();
+    }
+
+    this.draw();
+  }
+
+  draw() {
+    ctx.fillStyle = 'red';
+    drawBorderRect(this.posX, this.posY, 12, 12, 1);
+  }
+
+  setDir() {}
+}
+
+const enemies = [];
+const enemyCount = 10; // 적의 수
+
+for (let i = 0; i < enemyCount; i++) {
+  enemies.push(new Enemy());
+}
+
 function drawBorderRect(x, y, width, height, borderWidth) {
   // 캐릭터를 그리는 함수
   ctx.lineWidth = borderWidth;
   ctx.strokeStyle = 'black';
-  ctx.fillStyle = 'blue';
 
   ctx.fillRect(x, y, width, height);
   ctx.strokeRect(
@@ -25,8 +92,8 @@ function drawBorderRect(x, y, width, height, borderWidth) {
 
 function drawCharactor(x, y) {
   // 캐릭터 위치 업데이트
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.clearRect(prevPosX, prevPosY, 20, 20);
+  ctx.fillStyle = 'blue';
   drawBorderRect(x, y, 20, 20, 3);
 }
 
@@ -43,7 +110,12 @@ function updatePosition() {
   posX = Math.max(0, Math.min(canvas.width - 20, posX));
   posY = Math.max(0, Math.min(canvas.height - 20, posY));
 
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // 전체 캔버스를 지우고 다시 그립니다.
   drawCharactor(posX, posY);
+
+  for (let enemy of enemies) {
+    enemy.update();
+  }
   requestAnimationFrame(updatePosition);
 }
 
